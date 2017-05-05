e16dc2af9c44939487116b540968043f6d88fdd0
April Webster <awebster@us.ibm.com>
2017-05-05 09:01:04 -0700

    adding healthcheck endpoint for call to tone analyzer tone_chat endpoint

--- app.js
+++ app.js
@@ -35,9 +35,39 @@ const toneAnalyzer = new ToneAnalyzerV3({
   version_date: '2016-05-19',
 });
 
+const Cloudant = require('cloudant');
+const cloudant = new Cloudant({
+  account: process.env.CLOUDANT_USERNAME || 'ec1033fe-2664-40b3-81bd-9813bed3a1e9-bluemix',
+  password: process.env.CLOUDANT_PASSWORD || '50c00ec7df6384c127f0e199d280f66888022b5f353f6108cca8d95f2d2ea956',
+  plugin: 'promises',
+});
+
+const tonesAccuracyDb = cloudant.db.use('customer-tones-accuracy');
+
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/log/customer_tones_accuracy', (req, res) => {
+  console.log('customer_tones_accuracy endpoint called');
+  console.log('data is ' + JSON.stringify(req.body, 2, null));
+
+  const tonesAccuracyLogEntry = {
+    user_feedback: 'test',
+    tone_analyzer_response: 'test',
+    timestamp: (new Date(Date.now())).toISOString(),
+  };
+
+  tonesAccuracyDb.insert(tonesAccuracyLogEntry, (err, body) => {
+    if (err) {
+      return console.log('[db.insert] ', err.message);
+    }
+    console.log(body);
+    // const responseData = { collectDataResponse: body };
+    // console.log('app.js collect_data endpoint response to client front-end: ' + JSON.stringify(responseData, 2, null));
+    // res.send(responseData);
+  });
 });
 
 app.post('/api/tone_chat', (req, res, next) => {
@@ -46,6 +76,17 @@ app.post('/api/tone_chat', (req, res, next) => {
       return next(err);
     }
     return res.json(tone);
+  });
+});
+
+app.get('/healthchecks/tone_analyzer_request_test', (req, res) => {
+  toneAnalyzer.tone_chat({
+    utterances: [{ text: 'sad', user: 'customer' }],
+  }, (err) => {
+    if (err) {
+      return res.status(500);
+    }
+    return res.status(200);
   });
 });
 
